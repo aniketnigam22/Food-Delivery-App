17515d3922655559513ed3daa8cd4518f66e21a6
Aniket Nigam <aniketnigam22@gmail.com>
2024-06-10 17:41:10 +0530

    initial commit

--- App..js
+++ App..js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { View } from 'react-native';
 function App() {
   return (
     <>
-    
+      <View style={{backgroundColor:'red', flex:1}}></View>
     </>
   );
 }
