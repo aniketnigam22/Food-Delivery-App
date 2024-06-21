a8dec0dadf2a4b0da06ee6d8b95dbbf3a7084e0d
Aniket Nigam <aniknigam2@gmail.com>
2024-06-21 22:46:03 +0530

    commit has some problem related to package

--- App.js
+++ App.js
@@ -1,26 +1,65 @@
-import React from 'react'
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Alert } from 'react-native';
 import WelcomeScreen from './src/screens/LoginSignupScreen/WelcomeScreen';
 import LoginScreen from './src/screens/LoginSignupScreen/LoginScreen';
 import SignupScreen from './src/screens/LoginSignupScreen/SignupScreen';
-// import RootNavigation from './src/screens/RootNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import UserProfile from './src/screens/UserProfile';
 import ProductScreen from './src/screens/ProductScreen';
+// import messaging from '@react-native-firebase/messaging';
 
-//home branch at home
 function App() {
-
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    // Request permissions on iOS (Optional for Android)
+    async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
+
+    requestUserPermission();
+
+    // Foreground state messages
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    // Background and quit state messages
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    // Notification when app is opened from a quit state
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <>
-      {/* <RootNavigation/> */}
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerShown: false
-        }}
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
           initialRouteName='WelcomeScreen'
         >
           <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
@@ -29,14 +68,10 @@ function App() {
           <Stack.Screen name="HomeScreen" component={HomeScreen} />
           <Stack.Screen name="UserProfile" component={UserProfile} />
           <Stack.Screen name="ProductScreen" component={ProductScreen} />
-
         </Stack.Navigator>
       </NavigationContainer>
-
     </>
   );
 }
-
-
 
 export default App;
